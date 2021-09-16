3b921ecffc7a1e51fb6d829c87b6888ffe64a707
Manvi Goel <55682355+ManviGoel26@users.noreply.github.com>
2021-09-16 20:09:09 +0530

    Add files via upload

--- Assignment1/ElectivesAdvisory.pl
+++ Assignment1/ElectivesAdvisory.pl
@@ -5,9 +5,11 @@ main:-
     read(Branch),
     write("What is the ? (winter/ monsoon)"), nl,
     read(Sem),
-    writeln("Input the courses done. (Type 'stop' to complete)"),
+    writeln("Input the course code of the courses done. (Type 'stop' to complete) "),
     getPre(ListPrereq),!,
-    search_electives(Branch, Sem, ListPrereq).
+    writeln("Input the preference order of branch of course. (cse/ csai/ des/ ece/ mth/ bio/ oth)"),
+    getPre(ListBranch),!,
+    search_electives(Branch, Sem, ListPrereq, ListBranch).
 
 
 start:-
@@ -26,25 +28,24 @@ reset_courses.
 
 
 getPre([Pre|ListPre]):-
-    write("course code: "),
+    write("Enter: "),
     read(Pre),
     dif(Pre, stop),
     getPre(ListPre).
 
 getPre([]).
 
-
-
-search_electives(B, S, ListP):-
+search_electives(B, S, ListP, ListBranches):-
     suggestCoreCourses(B, S, ListP);
-    course_information(Code, Name, L1, L2, L3, S, _Any, L4),
-    contained_in(L1, ListP),
-    intersection(L3, ListP),
-    checkInterests(L4),
-    foreach(member(Element, L4), likes(Element, y)),
-    \+ suggestCourse(Name),
-    assertz(suggestCourse(Name)),
-    course_information(Code, Name, L1, L2, L3, S, oth, L4).
+    foreach(member(Element, ListBranches), \+assertElectives(S, Element, ListP)),
+    %course_information(Code, Name, L1, L2, L3, S, _Any, L4),
+    %contained_in(L1, ListP),
+    %intersection(L3, ListP),
+    %checkInterests(L4),
+    %foreach(member(Element, L4), likes(Element, y)),
+    %\+ suggestCourse(Name),
+    %assertz(suggestCourse(Name)),
+    course_information(_Code, _Name, _L1, _L2, _L3, S, some, _L4).
 
 
 intersection([],_).
@@ -55,6 +56,18 @@ intersection([Head|Tail],List) :-
 
 contained_in(L1, L2) :- maplist(contains(L2), L1).
 contains(L, X) :- member(X, L), !.
+
+
+assertElectives(Sem, Branch, ListP):-
+    course_information(_Code, Name, L1, _L2, L3, Sem, Branch, L4),
+    contained_in(L1, ListP),
+    intersection(L3, ListP),
+    checkInterests(L4),
+    foreach(member(Element, L4), likes(Element, y)),
+    \+ suggestCourse(Name),
+    assertz(suggestCourse(Name)), fail.
+    %course_information(Code, Name, L1, L2, L3, Sem, Branch, L4).
+
 
 assertCoreCourse(Code, _Sem, ListP):-
     contains(ListP, Code).
@@ -110,7 +123,7 @@ course_information(cse642, "Advanced Machine Learning", [mth201, cse343], [cse64
 course_information(cse631, "Advanced Operating Systems", [cse231], [], [], winter, cse, ["Unix Systems", "OS Kernels"]).
 course_information(cse201, "Advanced Programming", [cse101, cse102], [], [], monsoon, cse,["Object Oriented Programming"]).
 course_information(cse734, "Advanced Topics in Mobile Computing", [cse232, cse231], [], [], winter, cse, ["Challenges in Mobile Computing Domain"]).
-course_information(cse661, "Affective Computing", [cse101, cse102, cse201], [cse343, cse643], [], monsoon, csai; dse, "Affective Computing").
+course_information(cse661, "Affective Computing", [cse101, cse102, cse201], [cse343, cse643], [], monsoon, dse, "Affective Computing").
 course_information(cse222, "Algorithm Design and Analysis", [cse102], [], [], winter, cse, ["Design of Algorithms", "Proofs of correctness"]).
 course_information(cse546, "Applied Cryptography", [cse121], [], [], monsoon, cse, ["Perfect Security", "Cryptographic Protocols"]).
 course_information(cse529, "Approximation Algorithms", [cse222], [], [], winter, cse, ["Algorithms for NP Hard Problems"]).
@@ -121,28 +134,28 @@ course_information(cse640, "Collaborative Filtering", [mth100], [], [], monsoon,
 course_information(cse636, "Communication Networks", [mth201], [ece501], [], winter, cse, ["Communication Networking"]).
 course_information(cse301, "Compilers", [mth100, cse101, cse201, cse102], [], [], monsoon, cse, ["Compilers"]).
 course_information(cse421, "Complexity Theory", [cse102, mth201, cse322, cse121], [], [], monsoon, cse, ["Computaional Complexity"]).
-course_information(cse511, "Computer Architecture", [], [], [], monsoon, cse; ece,["Computer Architecture"]).
+course_information(cse511, "Computer Architecture", [], [], [], monsoon, cse,["Computer Architecture"]).
 course_information(cse333, "Computer Graphics", [cse101], [cse102], [], monsoon, cse, ["Computer Graphics"]).
 course_information(cse232, "Computer Networks", [cse101, cse231, cse222], [], [], monsoon, cse, ["Network Analysis"]).
 course_information(cse112, "Computer Organization", [ece111], [], [], winter, cse, ["Assembly Language"]).
 course_information(cse344, "Computer Vision", [mth100], [], [], winter, csai, ["Computer Vision"]).
-course_information(cse585, "Computing for Medicine", [], [], [], monsoon, cse; bio, ["Health Data"]).
+course_information(cse585, "Computing for Medicine", [], [], [], monsoon, cse, ["Health Data"]).
 course_information(cse606A, "Data Lifecycle Management", [cse343], [], [], monsoon, csai, ["Data Research", "Machine Learning"]).
 course_information(cse506, "Data Mining", [cse202, cse101, mth100, mth201], [], [], monsoon, cse, ["Patterns in Data"]).
 course_information(cse558, "Data Science",[cse101, cse343], [mth101], [], monsoon, csai,["Exploratary Data Analysis"]).
 course_information(cse102, "Data Structures And Algorithms", [cse101], [], [], winter, cse,["Data Strcutures"]).
 course_information(cse606, "Data Warehouse", [cse202], [], [], winter, cse, ["Data Warehouses"]).
 course_information(cse507, "Database System Implementation", [cse202, cse102], [], [], monsoon, cse, ["Database Systems"]).
-course_information(cse504, "Decision Procedures", [cse102; cse201, cse121; mth210], [], [], winter, cse, ["Decision Procedures"]).
+course_information(cse504, "Decision Procedures", [cse102, cse201, mth210], [], [], winter, cse, ["Decision Procedures"]).
 course_information(cse641, "Deep Learning", [], [cse343], [], winter, csai,["Neural Networks"]).
 course_information(cse501, "Designing Human Centered Systems", [], [], [des204], monsoon, cse, ["Design Paradigms"]).
 course_information(cse121, "Discrete Mathematics", [], [], [mth210], monsoon, cse, "Problem Solving Skills").
 course_information(cse530, "Distributed Systems: Concepts And Designs", [cse232, cse222], [], [], monsoon, cse, ["Distrbuted Systems"]).
-course_information(cse663, "EdgeAI", [cse343; cse342], [], [], winter, csai, ["On-Device AI"]).
+course_information(cse663, "EdgeAI", [cse343], [], [], winter, csai, ["On-Device AI"]).
 course_information(cse345, "Foundations of Computer Security", [], [cse232], [], monsoon, cse, ["Computer Security"]).
 course_information(cse502, "Foundations of Parallel Programming", [cse102, cse101, cse201], [], [], winter, cse, ["Parallel Programming"]).
 course_information(cse202, "Fundamentals of Database Management System", [cse102], [], [], winter, cse, ["Database Manegement"]).
-course_information(cse5GP, "Geometry Processing",[cse101], [cse102], [], winter; monsoon, cse, ["3D Geometry"]).
+course_information(cse5GP, "Geometry Processing",[cse101], [cse102], [], winter, cse, ["3D Geometry"]).
 course_information(cse560, "GPU Computing", [cse101], [cse102], [], winter, cse, ["Parallel Programming"]).
 course_information(cse656, "Information Integration and Applications", [cse202], [cse102], [], monsoon, cse, ["Integrating Information", "Database Management"]).
 course_information(cse508, "Information Retrieval", [cse202, cse201, cse102], [], [], winter, cse, ["Search Engines", "Information Retrieval"]).
@@ -152,7 +165,7 @@ course_information(cse140, "Introduction to Intelligent Systems", [], [], [], wi
 course_information(cse571, "Introduction to Media Computing", [], [], [], monsoon, cse, ["Digital Representation"]).
 course_information(cse101, "Introduction to Programming", [], [], [], monsoon, cse, ["Programming"]).
 course_information(cse622, "Introduction to Quantum Computing", [mth100], [mth102],[], winter, cse, ["Quantum Computing"]).
-course_information(cse555, "Introduction to Spatial Computing", [cse102, cse202], [], [], monsoon, cse; bio, ["Spatial Computing"]).
+course_information(cse555, "Introduction to Spatial Computing", [cse102, cse202], [], [], monsoon, cse, ["Spatial Computing"]).
 course_information(cse343, "Machine Learning", [mth100, mth201, mth101], [], [], monsoon, csai, ["Research", "Machine Learning"]).
 course_information(ece577, "Machine Learning Techniques for Real Time Control", [], [cse343, ece570], [], monsoon, cse, ["Machine Learning"]).
 course_information(cse559, "Mining Large Networks",[cse222, cse101, mth201], [cse343], [], winter, cse,["Graphs", "Networks"]).
@@ -172,7 +185,7 @@ course_information(cse634, "Parallel Runtimes for Modern Processors",[cse201, cs
 course_information(cse645, "Privacy and Security in Online Social Media", [], [], [], winter, cse, ["Priavcy", "Security"]).
 course_information(cse561, "Probabilistic Graphic Models", [mth201], [ece501], [], winter, cse, ["Probability"]).
 course_information(cse503, "Program Analysis", [cse201, cse102], [cse322], [], monsoon, cse, ["Analysis"]).
-course_information(cse584, "Program Verification", [cse121; mth210], [], [], monsoon, cse, ["Program Verification", "Correctness"]).
+course_information(cse584, "Program Verification", [mth210], [], [], monsoon, cse, ["Program Verification", "Correctness"]).
 course_information(cse523, "Randomized Algorithms", [cse222, cse121, mth201], [], [], monsoon, cse,["Algorithms"]).
 course_information(cse564, "Reinforcement Learning", [mth201], [], [], monsoon, csai, ["Reinforcement Learning"]).
 course_information(cse633, "Robotics", [], [], [], winter, csai, ["Robotics"]).
@@ -185,8 +198,8 @@ course_information(cse582, "Software Production Evolution and Maintenance",[], [
 course_information(cse572, "Speech and Audio Processing", [mth100, ece250], [mth201, ece351], [], monsoon, cse,["Audio"]).
 course_information(cse609, "Statistical Computation", [mth201], [], [], winter, cse, ["Statistics"]).
 course_information(cse342, "Statistical Machine Learning", [cse101, mth201], [], [], winter, csai, ["Statistics", "Machine Learning"]).
-course_information(cse322, "Theory Of Computation", [cse121; mth210], [], [], winter, cse, ["Automata Theory"]).
-course_information(cse516, "Thories of Deep Learning", [], [mth100, mth203, cse343; cse342], [], winter, csai,["Deep Learning"]).
+course_information(cse322, "Theory Of Computation", [mth210], [], [], winter, cse, ["Automata Theory"]).
+course_information(cse516, "Thories of Deep Learning", [], [mth100, mth203, cse343], [], winter, csai,["Deep Learning"]).
 course_information(cse524, "Theory of Modern cryptography", [], [], [], monsoon, cse, ["Cryptography"]).
 course_information(cse651, "Topics in Adaptive Cybersecurity", [cse345], [], [], monsoon, cse, ["Cybersecurity"]).
 course_information(cse701, "Topics in SE: AI in SE", [], [], [], monsoon, cse, ["Artificial Intelligence"]).
@@ -213,7 +226,7 @@ course_information(ece270, "Embedded Logic Design", [], [], [], monsoon, ece, ["
 course_information(ece230, "Fields and Waves", [mth100], [], [], winter, ece, ["Fields", "Waves"]).
 course_information(ece522, "Integrated Circuit Fabrication",[], [], [], monsoon, ece, ["Circuit Design"]).
 course_information(ece214, "Integrated Electronics", [ece111, ece113, des130], [], [], monsoon, ece, ["Electronics"]).
-course_information(ece519, "Intelligent Application Implementation on Hetrogeneous Platforms", [cse112], [], [], monsoon, ece; csai, ["Intelligent Systems"]).
+course_information(ece519, "Intelligent Application Implementation on Hetrogeneous Platforms", [cse112], [], [], monsoon, ece, ["Intelligent Systems"]).
 course_information(ece517, "Introduction to Nanoelectronics", [], [], [], winter, ece, ["Nanoelectronics"]).
 course_information(ece570, "Linear Systems Thoery", [], [], [], monsoon, ece, ["Linear Systems"]).
 course_information(ece520, "Low Voltage Analog Circuit Design", [ece315], [], [], monsoon, ece,["Circuit Design"]).
@@ -257,7 +270,7 @@ course_information(mth514, "Coding Theory", [mth100, mth212, mth513], [], [], wi
 course_information(mth517, "Combinatorial Optimization", [mth374], [], [], winter, mth, ["Optimization"]).
 course_information(mth311, "Combinatorics and Its Applications",[mth100], [], [], winter, mth, ["Combinatorics"]).
 course_information(mth341, "Complex Analysis", [mth240], [], [], monsoon, mth, ["Complex Numbers", "Analysis"]).
-course_information(mth577, "Convex Optimization", [mth100], [], [], winter, mth; csai, ["Convex Structures", "Optimization"]).
+course_information(mth577, "Convex Optimization", [mth100], [], [], winter, mth, ["Convex Structures", "Optimization"]).
 course_information(mth204, "Diffrential Equations", [mth203], [], [], winter, mth, ["Calculas"]).
 course_information(mth210, "Discrete Structure", [], [], [cse121], monsoon, mth, ["Discrete Maths"]).
 course_information(mth545, "Finite & SPectral Element Methods", [mth100, mth203, mth204], [], [], monsoon, mth,["Spectral Elements"]).
